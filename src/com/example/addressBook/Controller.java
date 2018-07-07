c094f3d2552715c3c489530d236833fe946bdbde
ifsohn <zdreitzler@gmail.com>
2018-07-07 11:02:31 -0400

    Added functionallity for the new contact dialog and a menu for opening it

--- src/com/example/addressBook/Controller.java
+++ src/com/example/addressBook/Controller.java
@@ -1,4 +1,53 @@
 package com.example.addressBook;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
+
+
 public class Controller {
+
+    @FXML
+    private BorderPane mainWindow;
+
+    @FXML
+    public void showNewDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainWindow.getScene().getWindow());
+        dialog.setTitle("Add a new Contact.");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("newDialog.fxml"));
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            NewDialogController controller = fxmlLoader.getController();
+            Contact contact = controller.processDialog();
+        }
+    }
+
+    @FXML
+    public void handleDeleteContact(){
+        //TODo
+        return;
+    }
+
+    @FXML
+    public void handleExit(){
+        //TODO
+        return;
+    }
 }
